34566433b6af1c24c8551f76b7919fd252286d8e
satyendra <skchandrawansi03@gmail.com>
2024-06-04 00:47:18 +0530

    Added profile pic

--- src/app/components/Navbar.js
+++ src/app/components/Navbar.js
@@ -4,6 +4,7 @@ import { usePathname } from "next/navigation";
 import { CiUser } from "react-icons/ci";
 import navLinks from "@/app/data/navLink"
 
+
 export default function Navbar() {
   const pathName = usePathname();
 
@@ -21,18 +22,22 @@ export default function Navbar() {
   }
   return (
     <>
-      <div className=" container mx-auto flex justify-between px-5 py-4 bg-red-100/50 shadow-xl rounded-full sticky top-2 left-2 right-2  backdrop-blur-sm z-10 md:py-5 md:top-1">
+      <div className=" container mx-auto flex justify-between px-5 py-4 bg-red-100/50 shadow-xl rounded-full sticky top-2 left-2 right-2  backdrop-blur-sm z-10 md:py-3 md:top-1">
         <div id="hamburger" className="md:hidden">
           <div className="line w-7 h-[3px] bg-primary m-1.5"></div>
           <div className="line w-5 h-[3px] bg-primary m-1.5"></div>
           <div className="line w-6 h-[3px] bg-primary m-1.5"></div>
         </div>
-
-        <div className="logo">
-          <h1 className="text-primary font-bold text-xl font-mono">
-            SATYENDRA
-          </h1>
-        </div>
+        <Link href="/">
+          <div className="logo flex justify-center items-center gap-2 ">
+            <div className="hidden  md:block">
+              <img className="w-10 rounded-full" src="/image.jpg" alt="" />
+            </div>
+            <h1 className="text-primary font-bold text-xl font-mono">
+              SATYENDRA
+            </h1>
+          </div>
+        </Link>
         {/* --------------------Nav link for md and lg devices-------------------- */}
         <div className="navLink hidden my-auto md:block ">
           <nav>
@@ -54,7 +59,8 @@ export default function Navbar() {
           className="absolute top-0 -left-52  bg-red-100/80 w-52 h-screen border shadow-xl backdrop-blur-sm transition-all duration-700 ease-in-out md:hidden"
         >
           <div className="logo flex justify-center border-b border-b-red-400 py-4 ">
-            <CiUser size={"4rem"} color="red" />
+            {/* <CiUser size={"4rem"} color="red" /> */}
+            <img className="w-28 rounded-full" src="/image.jpg" alt="" />
           </div>
           <nav>
             <ul className="">
